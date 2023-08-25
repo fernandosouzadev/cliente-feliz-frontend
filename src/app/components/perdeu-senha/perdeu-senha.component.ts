169fe558a8d79ffffd19273bcd9ae2821d326d13
Fernando Souza <fernandosouzadev@gmail.com>
2023-08-25 10:21:39 -0300

    Ajustes na recuperação de senha

--- src/app/components/perdeu-senha/perdeu-senha.component.ts
+++ src/app/components/perdeu-senha/perdeu-senha.component.ts
@@ -10,7 +10,7 @@ import { ToastService } from 'src/app/services/toast.service';
   styleUrls: ['./perdeu-senha.component.css'],
 })
 export class PerdeuSenhaComponent implements OnInit {
-  email: string;
+  email: string = '';
   formulario: FormGroup;
   isLoading: boolean = false;
 
